33ad0181b2e3339ccbc5b2ebc333b949ef446521
jesussarco <sarco.jesus@gmail.com>
2020-09-01 10:06:57 +0200

    Added test to application context.

--- src/test/java/guru/springframework/Spring5MvcRestApplicationTests.java
+++ src/test/java/guru/springframework/Spring5MvcRestApplicationTests.java
@@ -1,9 +1,9 @@
-package guru.springframework.spring5mvcrest;
+package guru.springframework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Spring5MvcRestApplication.class)
 class Spring5MvcRestApplicationTests {
 
     @Test
