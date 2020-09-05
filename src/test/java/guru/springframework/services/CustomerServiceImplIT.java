907591f18baa49176a4d217372d196873d8d6c6c
jesussarco <sarco.jesus@gmail.com>
2020-09-05 18:34:11 +0200

    Implemented Integration Test with DaraJPATest

---
+++ src/test/java/guru/springframework/services/CustomerServiceImplIT.java
@@ -0,0 +1,102 @@
+package guru.springframework.services;
+
+import guru.springframework.api.v1.mapper.CustomerMapper;
+import guru.springframework.api.v1.model.CustomerDTO;
+import guru.springframework.bootstrap.Bootstrap;
+import guru.springframework.domain.Customer;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by jesussarco on 05/09/2020
+ * Integration Test
+ */
+@DataJpaTest
+public class CustomerServiceImplIT {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    CustomerService customerService;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        System.out.println("Loading Customer Data");
+        System.out.println(customerRepository.findAll().size());
+
+        //setup data for testing
+        Bootstrap bootstrap = new Bootstrap(categoryRepository, customerRepository);
+        bootstrap.run(); //load data
+
+        customerService = new CustomerServiceImpl(CustomerMapper.INSTANCE, customerRepository);
+    }
+
+    @Test
+    public void patchCustomerUpdateFirstName() throws Exception {
+        String updatedName = "UpdatedName";
+        long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.getOne(id);
+        assertNotNull(originalCustomer);
+        //save original first name
+        String originalFirstName = originalCustomer.getFirstName();
+        String originalLastName = originalCustomer.getLastName();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setFirstName(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        Customer updatedCustomer = customerRepository.findById(id).orElseGet(null);
+
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getFirstName());
+        assertNotEquals(originalFirstName, updatedCustomer.getFirstName());
+        assertEquals(originalLastName, updatedCustomer.getLastName());
+    }
+
+    @Test
+    public void patchCustomerUpdateLastName() throws Exception {
+        String updatedName = "UpdatedName";
+        long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.getOne(id);
+        assertNotNull(originalCustomer);
+
+        //save original first/last name
+        String originalFirstName = originalCustomer.getFirstName();
+        String originalLastName = originalCustomer.getLastName();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setLastName(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        Customer updatedCustomer = customerRepository.findById(id).orElseGet(null);
+
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getLastName());
+        assertEquals(originalFirstName, updatedCustomer.getFirstName());
+        assertNotEquals(originalLastName, updatedCustomer.getLastName());
+    }
+
+    private Long getCustomerIdValue(){
+        List<Customer> customers = customerRepository.findAll();
+
+        System.out.println("Customers Found: " + customers.size());
+
+        //return first id
+        return customers.get(0).getId();
+    }
+}
