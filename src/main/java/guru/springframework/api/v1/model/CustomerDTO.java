b30eab7e3f2053df461c3a13f9feab3c6925aa49
jesussarco <sarco.jesus@gmail.com>
2020-09-07 16:57:10 +0200

    adding documentation to swagger ui

--- src/main/java/guru/springframework/api/v1/model/CustomerDTO.java
+++ src/main/java/guru/springframework/api/v1/model/CustomerDTO.java
@@ -2,6 +2,7 @@ package guru.springframework.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import guru.springframework.controllers.v1.CustomerController;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -11,7 +12,11 @@ import lombok.Data;
 public class CustomerDTO {
 
     private Long id;
+
+    @ApiModelProperty(value = "This is the first name", required = true)
     private String firstname;
+
+    @ApiModelProperty(required = true )
     private String lastname;
 
     @JsonProperty("customer_url")
--- src/main/java/guru/springframework/controllers/v1/CustomerController.java
+++ src/main/java/guru/springframework/controllers/v1/CustomerController.java
@@ -3,12 +3,15 @@ package guru.springframework.controllers.v1;
 import guru.springframework.api.v1.model.CustomerDTO;
 import guru.springframework.api.v1.model.CustomerListDTO;
 import guru.springframework.services.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by jesussarco on 01/09/2020
  */
+@Api(description = "This is my Customer Controller")
 @RestController
 @RequestMapping(CustomerController.BASE_URL)
 public class CustomerController {
@@ -20,6 +23,7 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @ApiOperation(value = "This will get a list of customers.", notes = "These are some notes about the API.")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public CustomerListDTO getAllCustomers() {
